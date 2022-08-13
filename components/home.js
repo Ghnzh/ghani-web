7e56aee68a5e13657f71c089599bd6d6a21efdc6
Ghnzh <ghani.alaziz17@gmail.com>
2022-08-13 21:53:52 +0700

    Update home.js

--- components/home.js
+++ components/home.js
@@ -20,7 +20,7 @@ export default function Home() {
             <svg className="w-7 h-7 fill-ijo" focusable="false" viewBox="0 0 24 24" aria-hidden="true">
               <path d="M20 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm-.4 4.25l-7.07 4.42c-.32.2-.74.2-1.06 0L4.4 8.25c-.25-.16-.4-.43-.4-.72 0-.67.73-1.07 1.3-.72L12 11l6.7-4.19c.57-.35 1.3.05 1.3.72 0 .29-.15.56-.4.72z"></path>
             </svg>
-            <span className="text-ijo text-22px pl-1">Say hello</span>
+            <span className="text-ijo md:text-22px pl-1">Say hello</span>
           </a>
         </div>
       </main>
